6133216f649a978b695da324d2c52b4951ad3c64
Nina <ninahovinmaa@gmail.com>
2020-09-16 14:01:45 +0200

    fixed stavfel

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(e) {
-    let btnSendn = document.getElementById("btnSend")
+    let btnSend = document.getElementById("btnSend")
     btnSend.addEventListener("click", function(e) {
         let message = document.getElementById("message");
         let new_message = document.createElement("p");
          new_message.innerHTML = message.value;
-         new_message.addEventListener("click", function(e) {
+         new_message.addEventListener("click", function(e) {            //
             this.remove();
          })
         let div = document.getElementById("root");
