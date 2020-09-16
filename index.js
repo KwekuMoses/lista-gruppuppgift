80bf440c29e115df01de8de609213acc6e756657
Nina <ninahovinmaa@gmail.com>
2020-09-16 13:54:48 +0200

    gruppuppgift med remove

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-    let elements = document.getElementsByTagName("li")
-    for (element of elements) {
-        element.addEventListener("click", function () {
+document.addEventListener("DOMContentLoaded", function(e) {
+    let btnSendn = document.getElementById("btnSend")
+    btnSend.addEventListener("click", function(e) {
+        let message = document.getElementById("message");
+        let new_message = document.createElement("p");
+         new_message.innerHTML = message.value;
+         new_message.addEventListener("click", function(e) {
             this.remove();
-        })
-    }
+         })
+        let div = document.getElementById("root");
+        div.appendChild(new_message);
+        message.value ="";
+    });
 })
