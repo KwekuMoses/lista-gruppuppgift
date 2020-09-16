93dae66d7bb738f1af329589569bb621d8fa3851
Kweku Moses <kweku.moses@hotmail.com>
2020-09-16 12:49:53 +0200

    lista med remove

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-document.addEventListener("DOMContentLoaded", function (e) {
+document.addEventListener("DOMContentLoaded", function () {
 
-    let button = document.getElementById("btnSend");
-    button.addEventListener("click", function () {
-        let message = document.getElementById("message");
-        let new_message = document.createElement("p");
-        new_message.innerHTML = message.value;
-
-        let div = document.getElementById("root");
-        div.appendChild(new_message);
-
-    })
-
+    let elements = document.getElementsByTagName("li")
+    for (element of elements) {
+        element.addEventListener("click", function () {
+            this.remove();
+        })
+    }
 })
